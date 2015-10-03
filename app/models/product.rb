f45f941fa6bbace8628df6c8b3ad8f001643e860
phanindhra <phani8996@gmail.com>
2015-10-03 12:29:48 +0530

    minor changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
 	has_one :user
 	has_many :comments
 	has_attached_file :image , :styles => {:small => '150x150>'} 
+
+	validates_attachment_presence :image
+	validates_attachment_size :image , :lessthan => 10.megabytes
+	validates_attachment_content_type :image , :content_type => ['image/jpeg','image/jpg','image/png']
 end
