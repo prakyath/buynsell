e083b14954227b174318efae6f90200bdc400a27
chaturasan <chatur4686@gmail.com>
2015-10-21 01:31:43 +0530

    comment_auth

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user
+  before_action :comment_admin,only: :destroy
   before_action :set_product, only: [:create,:destroy]
 
 
@@ -7,7 +8,7 @@ class CommentsController < ApplicationController
   @comment=set_product.comments.new(comment_params)	
   # @comment.message=comment_params[:message]
    
-  # @comment.user_id = current_user.id #or whatever is you session name
+   @comment.user_id = current_user.id #or whatever is you session name
   
   if @comment.save
     redirect_to @product
@@ -20,10 +21,11 @@ class CommentsController < ApplicationController
   
 
     def destroy
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to @product, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
+      @comment = set_product.comments.find(params[:id]) 
+      @comment.destroy
+      respond_to do |format|
+        format.html { redirect_to @product, notice: 'Comment was successfully destroyed.' }
+        format.json { head :no_content }
     end
   end
 
@@ -37,6 +39,13 @@ class CommentsController < ApplicationController
 
     def set_product
       @product = Product.find(params[:product_id]);
+    end
+
+    def comment_admin
+      @comment = set_product.comments.find(params[:id])
+      if(current_user.id != @comment.user_id )
+        redirect_to @product
+      end
     end
 
 end
