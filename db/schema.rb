12f0247288f83ef687d5c6852022056aa3b5626d
phani8996 <phani8996@gmail.com>
2015-12-12 09:12:25 +0000

    Image upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20151211160519) do
-=======
-ActiveRecord::Schema.define(version: 20151211073423) do
->>>>>>> 49e24328385acbce0a08a2354603a6bfeee5150b
 
   create_table "ads", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -37,11 +33,17 @@ ActiveRecord::Schema.define(version: 20151211073423) do
     t.integer  "product_id", limit: 4
   end
 
-<<<<<<< HEAD
   create_table "images", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer  "notifier_id", limit: 4
+    t.integer  "notifiee_id", limit: 4
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
   create_table "photos", force: :cascade do |t|
@@ -52,13 +54,6 @@ ActiveRecord::Schema.define(version: 20151211073423) do
     t.string   "image_content_type", limit: 255
     t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
-=======
-  create_table "notifications", force: :cascade do |t|
-    t.integer  "notifier_id", limit: 4
-    t.integer  "notifiee_id", limit: 4
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
->>>>>>> 49e24328385acbce0a08a2354603a6bfeee5150b
   end
 
   create_table "products", force: :cascade do |t|
@@ -76,7 +71,6 @@ ActiveRecord::Schema.define(version: 20151211073423) do
     t.string   "status",             limit: 255
   end
 
-<<<<<<< HEAD
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
@@ -95,6 +89,4 @@ ActiveRecord::Schema.define(version: 20151211073423) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-=======
->>>>>>> 49e24328385acbce0a08a2354603a6bfeee5150b
 end
