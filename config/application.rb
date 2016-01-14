5e2b053f9ae34a712c64fe29923e464091274ebe
phanindhra <phani8996@gmail.com>
2016-01-14 11:40:37 +0000

    server strating issue fixed

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Buynsell
   class Application < Rails::Application
-    config.web_console.whitelisted_ips = ' 14.139.160.234'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
