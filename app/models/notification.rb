70610ec823a4447fdd245b521658e714a97e15ad
phanindhra <phani8996@gmail.com>
2016-03-02 18:05:44 +0000

    notifications added

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,5 +3,6 @@ belongs_to :notifier, :class_name=>'User', :foreign_key=>'notifier_id'
 belongs_to :notifiee, :class_name=>'User', :foreign_key=>'notifiee_id'
 belongs_to :product
 belongs_to :comment
+belongs_to :Oauth
 
 end
--- app/models/notification_offer.rb
+++ app/models/notification_offer.rb
@@ -3,4 +3,5 @@ belongs_to :notifier, :class_name=>'User', :foreign_key=>'notifier_id'
 belongs_to :notifiee, :class_name=>'Product', :foreign_key=>'notifiee_id'
 belongs_to :product
 belongs_to :user
+belongs_to :Oauth
 end
