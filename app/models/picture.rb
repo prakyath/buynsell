e573a5f8752c5d92d3c226e8f8802ae3740b7fa2
phanindhra <phani8996@gmail.com>
2016-03-12 17:29:06 +0000

    Minor changes

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,11 +1,13 @@
 class Picture < ActiveRecord::Base
   belongs_to :product
-
   has_attached_file :image,
     :styles => { :thumb => "100x100#", :small => "250x250#", :medium=>"400x400#",:large => "600x600>" },
     :path => ":rails_root/public/images/Product_Pictures/:id/:style/:filename",
     :url  => "/images/Product_Pictures/:id/:style/:filename"
-validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_presence :image
+	validates_attachment_size :image , :less_than => 5.megabytes
+	validates :image, presence: true 
 end
 
 #medium for zoom galler,thumb for zoom gallery,small from index thumb
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,7 @@ class Product < ActiveRecord::Base
   has_many :comments , dependent: :destroy
   has_many :pictures, :dependent => :destroy
   accepts_nested_attributes_for :pictures 
+
 def self.search(query)
   where("name like ?", "%#{query}%") 
 end
